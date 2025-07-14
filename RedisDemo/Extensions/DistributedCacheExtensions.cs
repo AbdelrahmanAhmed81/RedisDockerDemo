0fae4ba43d7fdab46d19ac11dcdb182dbc897077
Abdelrahman Ahmed <97863124+AbdelrahmanAhmed81@users.noreply.github.com>
2025-07-14 05:03:58 +0300

    Add project files.

---
+++ RedisDemo/Extensions/DistributedCacheExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisDemo.Extensions
+{
+    public static class DistributedCacheExtensions
+    {
+        public static async Task SetRecord<T>(this IDistributedCache cache ,
+                                              string recordId ,
+                                              T data ,
+                                              TimeSpan? absoluteExpireTime = null ,
+                                              TimeSpan? unusedExpiredTime = null)
+        {
+            var cacheOptions = new DistributedCacheEntryOptions()
+            {
+                AbsoluteExpiration = DateTime.Now.Add(absoluteExpireTime ?? TimeSpan.FromSeconds(60)) ,
+                SlidingExpiration = unusedExpiredTime
+            };
+        }
+    }
+}
---
+++ RedisDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace RedisDemo
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IServiceCollection servicesCollection = ConfigureServices();
+            var servicesBuilder = servicesCollection.BuildServiceProvider();
+            await servicesBuilder.GetService<App>().RunAsync(args);
+        }
+
+        static IServiceCollection ConfigureServices()
+        {
+            var configuration = LoadConfiguration();
+
+            IServiceCollection services = new ServiceCollection();
+
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = configuration.GetConnectionString("Redis");
+                options.InstanceName = "RedisDemo_";
+            });
+
+            return services;
+        }
+
+        static IConfiguration LoadConfiguration()
+        {
+            //var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json" , optional: false , reloadOnChange: true);
+                //.AddJsonFile($"appsettings.{environmentName}.json" , false , true);
+
+            return builder.Build();
+        }
+    }
+}
